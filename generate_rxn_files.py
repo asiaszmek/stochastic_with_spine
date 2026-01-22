165fefb35c785a40e64538a1e0c60aa4571ad58d
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2026-01-22 11:19:37 +0100

    IP3 old age Rxn files and new basal

--- generate_rxn_files.py
+++ generate_rxn_files.py
@@ -56,11 +56,22 @@ flist_no_ER_spine_basal_old_age = [
     "Rxn_module_SOCE.xml",
     ]
 
+flist_no_ER_IP3_spine_basal_old_age = flist_no_ER_spine_basal_old_age + [
+    "Rxn_module_mGLuR.xml",
+    "Rxn_module_IP3R.xml"
+    ]
+
+
 flist_ER_spine_basal = flist_no_ER_spine_basal + [
     "Rxn_module_RyR3CaM_KeizerSmith.xml",
     "Rxn_module_SERCA3.xml",
     ]
 
+flist_ER_IP3_spine_basal_old_age = flist_no_ER_IP3_spine_basal_old_age+ [
+    "Rxn_module_RyR3CaM_KeizerSmith.xml",
+    "Rxn_module_SERCA3.xml",
+    "Rxn_module_RyR3.xml",
+    ]
 flist_ER_spine_basal_IP3 = flist_ER_spine_basal + [
     "Rxn_module_mGLuR.xml",
     "Rxn_module_IP3R.xml"
@@ -87,6 +98,10 @@ flist_no_ER_spine_stim_IP3 = flist_no_ER_spine_stim + [
 flist_ER_spine_stim_old_age = flist_ER_spine_basal_old_age + ["Rxn_module_CaCbuf.xml"]
     
 flist_no_ER_spine_stim_old_age = flist_no_ER_spine_basal_old_age + ["Rxn_module_CaCbuf.xml"]
+
+flist_ER_IP3_spine_stim_old_age = flist_ER_IP3_spine_basal_old_age + ["Rxn_module_CaCbuf.xml"]
+    
+flist_no_ER_IP3_spine_stim_old_age = flist_no_ER_IP3_spine_basal_old_age + ["Rxn_module_CaCbuf.xml"]
 
 
 flist_ER_spine_stim_Fluo4FF = flist_ER_spine_stim + [
@@ -250,6 +265,23 @@ if __name__ == "__main__":
     my_rxn_f = read_in_files(flist_ER_spine_basal_old_age)
     with  open("Rxn_spine_ER_bas_old_age.xml", "w") as f:
         f.write(etree.tostring(my_rxn_f, pretty_print=True).decode("utf-8"))
+
+    my_rxn_f = read_in_files(flist_no_ER_IP3_spine_basal_old_age)
+    with  open("Rxn_no_spine_ER_IP3_bas_old_age.xml", "w") as f:
+        f.write(etree.tostring(my_rxn_f, pretty_print=True).decode("utf-8"))
+
+    my_rxn_f = read_in_files(flist_ER_IP3_spine_basal_old_age)
+    with  open("Rxn_spine_ER_IP3_bas_old_age.xml", "w") as f:
+        f.write(etree.tostring(my_rxn_f, pretty_print=True).decode("utf-8"))
+
+    my_rxn_f = read_in_files(flist_no_ER_IP3_spine_stim_old_age)
+    with  open("Rxn_no_spine_ER_IP3_old_age.xml", "w") as f:
+        f.write(etree.tostring(my_rxn_f, pretty_print=True).decode("utf-8"))
+
+    my_rxn_f = read_in_files(flist_ER_IP3_spine_stim_old_age)
+    with  open("Rxn_spine_ER_IP3_old_age.xml", "w") as f:
+        f.write(etree.tostring(my_rxn_f, pretty_print=True).decode("utf-8"))
+
 
     my_rxn_f = read_in_files(flist_no_ER_spine_stim_old_age)
     with  open("Rxn_no_spine_ER_old_age.xml", "w") as f:

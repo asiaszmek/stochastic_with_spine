42972634da403ec66bc127c6accd955c7c0e71a6
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2026-06-24 11:09:21 +0200

    Add an equivalent of 100 Hz

--- scripts/max_amp_spine_max_amp_dend_ER_no_ER.py
+++ scripts/max_amp_spine_max_amp_dend_ER_no_ER.py
@@ -17,13 +17,8 @@ file_spine_ER_dend_ER = os.path.join("..", "1_spine_different_Ca_amp",
                                       "model_RyR2CaM_RyR3CaM_1.2_um_dend_spine_%s_ms_%s.h5")
 file_spine_ER_no_dend_ER = os.path.join("..", "1_spine_different_Ca_amp",
                                          "model_RyR3CaM_1.2_um_dend_spine_%s_ms_%s.h5")
-file_dend_ER_dend_Ca = os.path.join("..", "1_spine_different_Ca_amp_Ca_dend",
-                                  "model_Ca_dend_RyR2CaM_1.2_um_dend_spine_%s_ms_%s.h5")
-file_spine_ER_dend_ER_dend_Ca = os.path.join("..", "1_spine_different_Ca_amp_Ca_dend",
-                                      "model_Ca_dend_RyR2CaM_RyR3CaM_1.2_um_dend_spine_%s_ms_%s.h5")
 t_init = 3000
 output = "Ca"
-
 types = ["no ER", "ER no SA", "ER+SA", "SA"]
 markers = ["o", "s", "o", "^"]
 fillstyles = ["none", "none", "full", "none"]
@@ -36,10 +31,18 @@ spine_dend = "dend11"
 region_list = ["dend01", "dend02", "dend03", "dend04", "dend05", "dend06", "dend07","dend08", "dend08", "dend09", "dend10",
                "dend11", "dend12", "dend13", "dend14", "dend15", "dend16", "dend17",  "dend18", "dend18", "dend19", "dend20",
                "dend21"]
-spine_idx = 21
+
 if __name__ == '__main__':
-    rollo = False
-    dirs = [file_no_ER_ctrl, file_dend_ER_ctrl, file_spine_ER_dend_ER, file_spine_ER_no_dend_ER]
-    fig = utils.make_distance_figs(dirs, stim_dict, output, types, markers,
-                       fillstyles, length=42, spine_idx=21, t_init=t_init)
-    fig.savefig("Spatial_spread_ER_no_ER_%s.png" % rollo, dpi=100, bbox_inches="tight")
+    directories = [file_no_ER_ctrl, file_dend_ER_ctrl, file_spine_ER_dend_ER, file_spine_ER_no_dend_ER]
+    fig1, fig2, fig3 = utils.max_vs_auc_head_neck_dend(directories,
+                                                       stim_dict, output,
+                                                       types, markers,
+                                                       fillstyles,
+                                                       length=42,
+                                                       which_dend=["dend11"],
+                                                       t_init=3000)
+       
+
+    fig1.savefig("ER_no_ER_aucdend_auchead.png", dpi=100, bbox_inches="tight")
+    fig2.savefig("ER_no_ER_maxdend_maxhead.png", dpi=100, bbox_inches="tight")
+    fig3.savefig("ER_no_ER_aucneck_auchead.png", dpi=100, bbox_inches="tight")
